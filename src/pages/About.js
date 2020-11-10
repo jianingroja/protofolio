0cee5ac85c91b5e1ec5a275278c57fefb81c6aa1
Jianing <jianingw321@outlook.com>
2020-11-10 23:32:06 +0800

    about and resume page setup

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,6 +2,31 @@ const About = () => {
   return (
     <div>
       <h1>About</h1>
+      <div className="about">
+        <div className="about-detail">
+          <h3>姓名</h3>
+          <p>Lorem ipsum dolor sit amet.</p>
+        </div>
+        <div className="about-detail">
+          <h3>爱好</h3>
+          <p>Lorem ipsum dolor sit amet.</p>
+        </div>
+        <div className="about-detail">
+          <h3>状态</h3>
+          <p>Lorem ipsum dolor sit amet.</p>
+        </div>
+        <div className="about-detail">
+          <h3>社交账号</h3>
+          <p>Lorem ipsum dolor sit amet.</p>
+        </div>
+        <div className="about-detail">
+          <h3>人生梦想</h3>
+          <p>Lorem ipsum dolor sit amet.</p>
+        </div>
+      </div>
+      <div className="about-image">
+        <img src="" alt="" />
+      </div>
     </div>
   );
 };
--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -2,6 +2,110 @@ const Resume = () => {
   return (
     <div>
       <h1>Resume</h1>
+      <div className="resume-image">
+        <img src="" alt="" />
+      </div>
+      <div className="resume">
+        <div className="resume-detail">
+          <div className="resume-education">
+            <h2>教育经历</h2>
+            <div className="education-detail">
+              <h3>对外经济贸易大学</h3>
+              <ul>
+                <li>课程</li>
+                <li>荣誉</li>
+              </ul>
+            </div>
+            <div className="education-detail">
+              <h3>马德里靠米亚斯天主教大学</h3>
+              <ul>
+                <li>课程</li>
+                <li>荣誉</li>
+              </ul>
+            </div>
+          </div>
+          <div className="resume-experience">
+            <h2>工作和社会实践</h2>
+            <div className="expereience-detail">
+              <h3>北京魔力耳朵科技公司——人力资源</h3>
+              <ul>
+                <li>负责1</li>
+                <li>负责2</li>
+              </ul>
+            </div>
+            <div className="expereience-detail">
+              <h3>塞尔维亚贝尔格莱德国际青年旅社</h3>
+              <ul>
+                <li>负责1</li>
+                <li>负责2</li>
+              </ul>
+            </div>
+            <div className="expereience-detail">
+              <h3>阿尔巴尼亚芙罗拉国际青年旅社</h3>
+              <ul>
+                <li>负责1</li>
+                <li>负责2</li>
+              </ul>
+            </div>
+            <div className="expereience-detail">
+              <h3>BeHive社交巢——新媒体运营（微信）</h3>
+              <ul>
+                <li>负责1</li>
+                <li>负责2</li>
+              </ul>
+            </div>
+          </div>
+          <div className="resume-volunteer">
+            <h2>志愿服务和社团活动</h2>
+            <div className="volunteer-detail">
+              <h3>对外经济贸易大学附属小学英语角老师</h3>
+              <ul>
+                <li>干了这个</li>
+                <li>干了那个</li>
+              </ul>
+            </div>
+            <div className="volunteer-detail">
+              <h3>对外经济贸易大学附属小学英语角老师</h3>
+              <ul>
+                <li>干了这个</li>
+                <li>干了那个</li>
+              </ul>
+            </div>
+            <div className="volunteer-detail">
+              <h3>对外经济贸易大学附属小学英语角老师</h3>
+              <ul>
+                <li>干了这个</li>
+                <li>干了那个</li>
+              </ul>
+            </div>
+            <div className="volunteer-detail">
+              <h3>对外经济贸易大学附属小学英语角老师</h3>
+              <ul>
+                <li>干了这个</li>
+                <li>干了那个</li>
+              </ul>
+            </div>
+          </div>
+          <div className="resume-skills">
+            <h2>技能</h2>
+            <ul>
+              <li>英语</li>
+              <li>html</li>
+              <li>react</li>
+              <li>blaaa</li>
+            </ul>
+          </div>
+          <div className="resume-motivation">
+            <h2>与多抓鱼的契合之处</h2>
+            <ul>
+              <li>自我驱动，学习能力</li>
+              <li>英语好，</li>
+              <li>认同多抓鱼经营理念</li>
+              <li>多抓鱼小程序、App深度用户，熟悉产品结构</li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
